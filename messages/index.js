597482cf8fbed233aa2670533fb226118f2e0120
Aaron Friel <mayreply@aaronfriel.com>
2016-11-21 01:50:57 -0600

    Add LUIS code for text messaging.

--- messages/index.js
+++ messages/index.js
@@ -1,54 +1,129 @@
 /*-----------------------------------------------------------------------------
-This template demonstrates how to use an IntentDialog with a LuisRecognizer to add 
-natural language support to a bot. 
-For a complete walkthrough of creating this type of bot see the article at
+This template demonstrates how to use an IntentDialog with a LuisRecognizer to add
+natural language support to a bot.
+For a compconste walkthrough of creating this type of bot see the article at
 http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 -----------------------------------------------------------------------------*/
-"use strict";
-var builder = require("botbuilder");
-var botbuilder_azure = require("botbuilder-azure");
+'use strict';
+const builder = require('botbuilder');
+const botbuilderAzure = require('botbuilder-azure');
 
-var useEmulator = (process.env.NODE_ENV == 'development');
+const useEmulator = (process.env.NODE_ENV === 'development');
+const moment = require('moment');
 
-var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    stateEndpoint: process.env['BotStateEndpoint'],
-    openIdMetadata: process.env['BotOpenIdMetadata']
-});
+const connector = useEmulator ?
+    new builder.ChatConnector() :
+    new botbuilderAzure.BotServiceConnector({
+      appId: process.env.MicrosoftAppId,
+      appPassword: process.env.MicrosoftAppPassword,
+      stateEndpoint: process.env.BotStateEndpoint,
+      openIdMetadata: process.env.BotOpenIdMetadata,
+    });
 
-var bot = new builder.UniversalBot(connector);
+const bot = new builder.UniversalBot(connector);
 
 // Make sure you add code to validate these fields
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'api.projectoxford.ai';
+const luisAppId = process.env.LuisAppId;
+const luisAPIKey = process.env.LuisAPIKey;
+const luisAPIHostName = process.env.LuisAPIHostName || 'api.projectoxford.ai';
 
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+const luisModelUrl = `https://${luisAPIHostName}/luis/v1/application?id=${luisAppId}&subscription-key=${luisAPIKey}`;
 
 // Main dialog with LUIS
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+const recognizer = new builder.LuisRecognizer(luisModelUrl);
+const intents = new builder.IntentDialog({ recognizers: [recognizer] })
 /*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
-.matches('None', (session, args) => {
-    session.send('Hi! This is the None intent handler. You said: \'%s\'.', session.message.text);
-})
+.matches('TextGroup',
+    [(session, args, next) => {
+        // Resolve and store any entities passed from LUIS.
+      const target = builder.EntityRecognizer.findEntity(args.entities, 'TextTarget');
+      const timeString = builder.EntityRecognizer.findEntity(args.entities, 'TextScheduledTime');
+      let time = null;
+      if (timeString) {
+        time = builder.EntityRecognizer.recognizeTime(timeString.entity);
+        time = builder.EntityRecognizer.parseTime(timeString.entity);
+      }
+
+      const messageEntity = builder.EntityRecognizer.findEntity(args.entities, 'TextMessage');
+      let message = null;
+      if (messageEntity) {
+        message = session.message.text.substr(
+          messageEntity.startIndex,
+          messageEntity.endIndex - messageEntity.startIndex);
+      }
+
+      const textMessage = session.dialogData.textMessage = {
+        target: target ? target.entity : null,
+        time: time ? time.resolution.ref : null,
+        message: message ? message : null,
+      };
+
+      // Prompt for target
+      if (!textMessage.target) {
+        builder.Prompts.text(session, 'Who would you like to message?');
+      } else {
+        next();
+      }
+    },
+    (session, results, next) => {
+      const textMessage = session.dialogData.textMessage;
+      if (results.response) {
+        textMessage.target = results.response;
+      }
+
+      if (textMessage.target && !textMessage.time) {
+        builder.Prompts.time(session, 'What time would you like to send the message?');
+      } else {
+        next();
+      }
+    },
+    (session, results, next) => {
+      const textMessage = session.dialogData.textMessage;
+      if (results.response) {
+        const time = builder.EntityRecognizer.resolveTime([results.response]);
+        textMessage.time = time;
+      }
+
+      // Prompt for message (title will be blank if the user said cancel)
+      if (textMessage.target && textMessage.time && !textMessage.message) {
+        builder.Prompts.time(session, 'What would you like to send in the message?');
+      } else {
+        next();
+      }
+    },
+    (session, results) => {
+      const textMessage = session.dialogData.textMessage;
+      if (results.response) {
+        textMessage.message = results.response;
+      }
+
+      if (textMessage.target && textMessage.time && textMessage.message) {
+        const formattedTime = moment(textMessage.time).calendar();
+        const target = textMessage.target;
+        const message = textMessage.message;
+
+        session.send(`Okay! ${formattedTime} we will send ${target} "${message}"`);
+      } else {
+        session.send('Ok... no problem.');
+      }
+    }]
+)
 .onDefault((session) => {
-    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+  session.send('Sorry, I did not understand \'%s\'.', session.message.text);
 });
 
-bot.dialog('/', intents);    
+bot.dialog('/', intents);
 
 if (useEmulator) {
-    var restify = require('restify');
-    var server = restify.createServer();
-    server.listen(3978, function() {
-        console.log('test bot endpont at http://localhost:3978/api/messages');
-    });
-    server.post('/api/messages', connector.listen());    
+  const restify = require('restify');
+  const server = restify.createServer();
+  server.listen(3978, () => {
+    console.log('test bot endpont at http://localhost:3978/api/messages');
+  });
+  server.post('/api/messages', connector.listen());
 } else {
-    module.exports = { default: connector.listen() }
+  module.exports = { default: connector.listen() };
 }
 
