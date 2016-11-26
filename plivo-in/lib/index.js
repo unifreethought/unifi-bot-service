35eb2f72ed8886ac0a37e70bfa220e034479de16
Aaron Friel <mayreply@aaronfriel.com>
2016-11-26 00:06:22 -0600

    Add context to bot constructor, use SMS channel.

--- plivo-in/lib/index.js
+++ plivo-in/lib/index.js
@@ -19,45 +19,45 @@ const settings = {
         plivoNumber: process.env.PlivoNumber,
     },
 };
-const connector = useEmulator
-    ? new UnifiConnector_1.UnifiConnector(settings)
-    : new UnifiConnectorAzure_1.UnifiConnectorAzure(settings);
-const bot = new builder.UniversalBot(connector);
-const intents = new builder.IntentDialog()
-    .matches(/^@unifibot help/i, (session) => {
-    session.send("Hello! I\"m the UNIFI Bot. Right now my functions are:\n\n"
-        + "1. Sending text messages (SMS) to groups of users. e.g.: "
-        + "Text members at 3PM \"UNIFI Forum tonight at 6 behind Chat\'s\"!");
-})
-    .matches(/^@unifibot echo/, (session, args) => {
-    const filter = new RegExp('^@unifibot echo (.*)');
-    const matches = session.message.text.match(filter);
-    if (matches) {
-        session.send(matches[1]);
-    }
-})
-    .onDefault((session) => {
-    const message = new builder.Message()
-        .address(JSON.parse(process.env.SlackAddress))
-        .text(session.message.text);
-    bot.send(message);
-});
-
-bot.dialog("/", intents);
 if (useEmulator) {
     const server = restify.createServer();
     server.listen(3978, () => {
         console.log("test bot endpont at http://localhost:3978/api/plivo-in");
     });
+    let context = { log: console.log };
+    let connector = new UnifiConnector_1.UnifiConnector(settings, context);
+    let bot = makeBot(connector);
     server.post("/api/plivo-in", connector.listen());
 }
 else {
-    module.exports = { default: connector.listen() };
+    module.exports = (context, req) => {
+        let connector = new UnifiConnectorAzure_1.UnifiConnectorAzure(settings, context);
+        let bot = makeBot(connector);
+        connector.listenAzure(req);
+    };
 }
-/*
-POST https://unifi-bot-service.azurewebsites.net/api/plivo-in?code=3uy26msc0ipafhy67c1xqyxz0loz7kdn9xx41
-
-To=13192149770&From=13196106722&Text=%40unifibot%20help&Type=sms&MessageUUID=38a67f51-b1f3-11e6-8c60-02daa5941325
-
-*/
+function makeBot(connector) {
+    const bot = new builder.UniversalBot(connector);
+    const intents = new builder.IntentDialog()
+        .matches(/^@unifibot help/i, (session) => {
+        session.send("Hello! I\"m the UNIFI Bot. Right now my functions are:\n\n"
+            + "1. Sending text messages (SMS) to groups of users. e.g.: "
+            + "Text members at 3PM \"UNIFI Forum tonight at 6 behind Chat\'s\"!");
+    })
+        .matches(/^@unifibot echo/, (session, args) => {
+        const filter = new RegExp('^@unifibot echo (.*)');
+        const matches = session.message.text.match(filter);
+        if (matches) {
+            session.send(matches[1]);
+        }
+    })
+        .onDefault((session) => {
+        const message = new builder.Message()
+            .address(JSON.parse(process.env.SlackAddress))
+            .text(session.message.text);
+        bot.send(message);
+    });
+    bot.dialog("/", intents);
+    return bot;
+}
 //# sourceMappingURL=index.js.map
--- plivo-in/src/index.ts
+++ plivo-in/src/index.ts
@@ -1,4 +1,5 @@
-import { IUniversalBotSettings } from './bots/botbuilder';
+import { UniversalBot } from 'botbuilder';
+import { IConnector, IUniversalBotSettings } from './bots/botbuilder';
 import { ConnectionType, IUnifiConnectorSettings, UnifiConnector } from './bots/UnifiConnector';
 import { UnifiConnectorAzure } from './bots/UnifiConnectorAzure';
 
@@ -9,11 +10,11 @@ import * as restify from "restify";
 const useEmulator = (process.env.NODE_ENV === "development");
 
 const settings: IUnifiConnectorSettings = {
-  chatSettings: <any> {
-      appId: process.env.MicrosoftAppId,
-      appPassword: process.env.MicrosoftAppPassword,
-      openIdMetadata: process.env.BotOpenIdMetadata,
-      stateEndpoint: process.env.BotStateEndpoint,
+  chatSettings: <any>{
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+    openIdMetadata: process.env.BotOpenIdMetadata,
+    stateEndpoint: process.env.BotStateEndpoint,
   },
   connectedTo: ConnectionType.Plivo,
   plivoSettings: {
@@ -23,50 +24,50 @@ const settings: IUnifiConnectorSettings = {
   },
 };
 
-const connector = useEmulator
-  ? new UnifiConnector(settings)
-  : new UnifiConnectorAzure(settings);
-
-const bot = new builder.UniversalBot(connector);
-
-const intents = new builder.IntentDialog()
-.matches(/^@unifibot help/i, (session) => {
-    session.send("Hello! I\"m the UNIFI Bot. Right now my functions are:\n\n"
-      + "1. Sending text messages (SMS) to groups of users. e.g.: "
-      + "Text members at 3PM \"UNIFI Forum tonight at 6 behind Chat\'s\"!");
-})
-.matches(/^@unifibot echo/, (session, args) => {
-    const filter = new RegExp('^@unifibot echo (.*)');
-    const matches = session.message.text.match(filter);
-    if (matches) {
-      session.send(matches[1]);
-    }
-})
-.onDefault((session) => {
-  const message = new builder.Message()
-    .address(JSON.parse(process.env.SlackAddress))
-    .text(session.message.text);
-  bot.send(message);
-});
-
-bot.on('trigger')
-
-bot.dialog("/", intents);
-
 if (useEmulator) {
   const server = restify.createServer();
   server.listen(3978, () => {
     console.log("test bot endpont at http://localhost:3978/api/plivo-in");
   });
 
+  let context = { log: console.log };
+  let connector = new UnifiConnector(settings, context);
+  let bot = makeBot(connector);
+
   server.post("/api/plivo-in", connector.listen());
 } else {
-  module.exports = { default: connector.listen() };
+  module.exports = (context, req) => {
+      let connector = new UnifiConnectorAzure(settings, context);
+      let bot = makeBot(connector);
+
+      connector.listenAzure(req);
+  };
 }
 
-/*
-POST https://unifi-bot-service.azurewebsites.net/api/plivo-in?code=3uy26msc0ipafhy67c1xqyxz0loz7kdn9xx41
+function makeBot(connector: IConnector): UniversalBot {
+  const bot = new builder.UniversalBot(connector);
 
-To=13192149770&From=13196106722&Text=%40unifibot%20help&Type=sms&MessageUUID=38a67f51-b1f3-11e6-8c60-02daa5941325
+  const intents = new builder.IntentDialog()
+    .matches(/^@unifibot help/i, (session) => {
+      session.send("Hello! I\"m the UNIFI Bot. Right now my functions are:\n\n"
+        + "1. Sending text messages (SMS) to groups of users. e.g.: "
+        + "Text members at 3PM \"UNIFI Forum tonight at 6 behind Chat\'s\"!");
+    })
+    .matches(/^@unifibot echo/, (session, args) => {
+      const filter = new RegExp('^@unifibot echo (.*)');
+      const matches = session.message.text.match(filter);
+      if (matches) {
+        session.send(matches[1]);
+      }
+    })
+    .onDefault((session) => {
+      const message = new builder.Message()
+        .address(JSON.parse(process.env.SlackAddress))
+        .text(session.message.text);
+      bot.send(message);
+    });
 
-*/
+  bot.dialog("/", intents);
+
+  return bot;
+}
